c1f853570ba67d708fe42e17a1d9e4bf4bef05a0
Aleksey Smirnov <i@smirnov-alexsey.ru>
2023-01-03 16:29:36 +0300

    test

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,38 @@
-class User {
-    role?: 'admin' | 'user';
-
-    constructor(private name: string) {
-    }
+interface Data {
+    group: number;
+    name: string;
 }
 
-function createUser(user: User) {
-    switch (user.role) {
-        case 'admin':
-            const a = 2;
-            break;
-        case 'user':
-            return true;
-    }
+const data: Data[] = [
+
+    { group: 1, name: 'a' },
+
+    { group: 1, name: 'b' },
+
+    { group: 2, name: 'c' },
+
+];
+
+interface IGroup<T> {
+    [key: string] : T[];
 }
 
-interface ICheck {
-    [check: string]: boolean;
+type key = string | number | symbol;
+
+function group<T extends Record<key, any>>(data: T[], key: keyof T): IGroup<T> {
+    return data.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+        const itemKey = item[key];
+        let curEl = map[itemKey];
+        if (Array.isArray(curEl)) {
+            curEl.push(item);
+        } else {
+            curEl = [item];
+        }
+
+        map[itemKey] = curEl;
+        return map;
+    }, {});
 }
 
-const c: ICheck = {};
-const d = c['drive'];
+const res = group<Data>(data, 'group');
+console.log(res);
